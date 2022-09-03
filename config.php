e201409d066a5864be6aa5b5de3c22c4d16ce80f
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2022-09-03 17:16:09 -0300

    added config.php file

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'database' => [
+        'name' => 'php_practitioner',
+        'username' => 'root',
+        'password' => '25fev05Dunga!',
+        'connection' => 'mysql:host=127.0.0.1',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    ]
+];
--- database/Connection.php
+++ database/Connection.php
@@ -2,12 +2,17 @@
 
 class Connection
 {
-    public static function make()
+    public static function make($config)
     {
         try {
-            return new PDO('mysql:host=127.0.0.1;dbname=php_practitioner', 'root', '25fev05Dunga!');
+            return new PDO(
+                $config['connection'] . ';dbname=' . $config['name'],
+                $config['username'],
+                $config['password'],
+                $config['options']
+            );
         } catch (PDOException $e) {
-            die($e->getMessage());
+            var_dump($e->getMessage());
         }
     }
 }
